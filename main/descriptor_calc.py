fdd28649ab04a25cc51cd8c4f142d5ddaa357496
Aayush Arya <tomriddle257@gmail.com>
2024-04-30 23:02:21 +0200

    Method for making violin plots with descriptors

--- main/descriptor_calc.py
+++ main/descriptor_calc.py
@@ -1,3 +1,4 @@
+import sys
 import localcider
 import pandas as pd
 import numpy as np
@@ -12,13 +13,31 @@ plt.rcParams['text.usetex'] = True
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = ['Computer Modern Serif']
 
+slice_after = 50
+args = sys.argv
+if len(args) > 1:
+		slice_after = int(args[1])
 
-seqs_init = pd.read_csv("../data/seq_b22_data_init.csv").iloc[:50]
-seqs_init50_r1 = pd.read_csv("../data/new/init50/seq_b22_data_50_r1.csv")
+seqs_init = pd.read_csv("../data/seq_b22_data_init.csv").iloc[:slice_after]
+'''seqs_init50_r1 = pd.read_csv("../data/new/init50/seq_b22_data_50_r1.csv")
 seqs_init50_r2 = pd.read_csv("../data/new/init50/seq_b22_data_50_r2.csv")
-seqs_init50_r3 = pd.read_csv("../data/new/init50/seq_b22_data_init50_r3.csv")
+seqs_init50_r3 = pd.read_csv("../data/new/init50/seq_b22_data_init50_r3.csv")'''
 
-seqs_init100_r1 = pd.read_csv("../data/new/init100/seq_b22_init100_r1.csv")
+init50_r1_seqs = pd.read_csv('../data/new/init50/seq_b22_data_50_r1.csv')
+init50_r2_seqs = pd.read_csv('../data/new/init50/seq_b22_data_50_r2.csv')
+init50_r3_seqs = pd.read_csv('../data/new/init50/seq_b22_data_init50_r3.csv')
+init50_r4_seqs = pd.read_csv('../data/new/init50/seq_b22_opt50_r4.csv')
+init50_r5_seqs = pd.read_csv('../data/new/init50/seq_b22_data_init50_r5.csv')
+
+init100_r1_seqs = pd.read_csv('../data/new/init100/seq_b22_init100_r1.csv')
+init150_r1_seqs = pd.read_csv('../data/new/init150/seq_init150_r1.csv')
+init200_r1_seqs = pd.read_csv('../data/new/init200/seq_b22_data_init200_r1.csv')
+
+init100_r2_seqs = pd.read_csv('../data/new/init100/seq_b22_init100_r2.csv')
+init150_r2_seqs = pd.read_csv('../data/new/init150/seq_b22_init150_r2.csv')
+init200_r2_seqs = pd.read_csv('../data/new/init200/seq_b22_init200_r2.csv')
+
+#seqs_init100_r1 = pd.read_csv("../data/new/init100/seq_b22_init100_r1.csv")
 #seqs_init100_r2 = pd.read_csv('')
 
 
@@ -66,6 +85,52 @@ def estimate_descriptors(df):
 	return combined_df
 
 
+def make_violin_plots(dataframe_list):
+	"""
+	Instead of a scatter plot, turn those into violin plots
+	"""
+	fig, axes = plt.subplots(3,3, figsize=(9,9))
+	# the name of the column in the dataframes, corresponding to the descriptors
+	# of interest
+	descriptor_col_names = ['Fraction Charged Residues', 'Net Charge Per Residue',
+						 'Fraction Negatively Charged', 'Fraction Positively Charged',
+						 'Fraction Expanding', 'Disorder Promoting Residues', 
+						 'Kappa', 'Omega', 'Mean Hydropathy']
+	descriptor_arr_dict = {}
+	# for violin plots, we need a single array containing all iters
+	# start by combining df's
+	iterations = np.arange(len(dataframe_list))
+	for i, iteration_data in enumerate(dataframe_list):
+		for desc in descriptor_col_names:
+			if desc in descriptor_arr_dict:
+				descriptor_arr_dict[desc].append(iteration_data[desc].to_numpy())
+			else:
+				descriptor_arr_dict[desc] = [iteration_data[desc].to_numpy()]
+	
+	cmap = matplotlib.colormaps['Spectral_r'] # Spectral_r also looked fine
+	colors = cmap(np.arange(0, 1, 1/len(iterations)), bytes=True)/255
+
+	# now, for each descriptor, place the data in one of the panels as a violin plot
+	for desc, (i, j) in zip(descriptor_col_names, np.ndindex(3,3)):
+		axes[i,j].set_prop_cycle(color=colors)
+		for it in iterations:
+			parts = axes[i, j].violinplot(descriptor_arr_dict[desc][it], [it], showmedians=True,
+								showextrema=True, bw_method=0.2)
+			axes[i,j].scatter(it, np.median(descriptor_arr_dict[desc][it]), c=colors[it], marker='+', s=128)
+			# customize plot
+			for pc in parts['bodies']:
+				#pc.set_facecolor(colors[it])
+				pc.set_edgecolor('k')
+				#pc.set_alpha(1)
+
+		axes[i, j].set_xlabel('Iteration', fontsize=13)
+		axes[i, j].set_xticks(iterations)
+		axes[i, j].set_ylabel(desc, fontsize=13)
+		axes[i, j].tick_params(which='major', axis='both', length=6, labelsize=13)
+	plt.tight_layout()
+	plt.savefig("descriptors_violin.png", dpi=300)
+
+
 def make_plots(dataframe_list):
 	# haha lol
 	fig, axes = plt.subplots(3, 3, figsize=(9,9))
@@ -106,8 +171,8 @@ def make_plots(dataframe_list):
 					c=seq_set['Numeric Label'], marker='$\circ$')
 		axes[2,2].set_ylabel('Mean Hydropathy')
 
-		cax = plt.axes((0.85, 0.1, 0.075, 0.8))
-		fig.colorbar(axes[2,2], cax=cax)
+		#cax = plt.axes((0.85, 0.1, 0.075, 0.8))
+		#fig.colorbar(axes[2,2], cax=cax)
 	for i,j in np.ndindex((3,3)):
 		axes[i,j].set_xlabel('Iteration')
 	plt.tight_layout()
@@ -117,10 +182,15 @@ def make_plots(dataframe_list):
 
 def compare_b22_distrib(dataframe_list):
 	fig, ax = plt.subplots(figsize=(6,6))
+
+	cmap = matplotlib.colormaps['RdYlBu']
+	colors = cmap(np.arange(0, 1, 1/len(dataframe_list)), bytes=True)/255
+
 	for i, df in enumerate(dataframe_list):
 		if 'Numeric Label' in df.columns:
-			ax.hist(-df['Numeric Label'], bins='fd', alpha = 0.4, ec='k', lw=0.25, label=f'Iteration {i}')
-			ax.axvline(x=-np.average(df['Numeric Label']), ymin=0, ymax=1, ls='--', c='gray', lw=0.75)
+			ax.hist(-df['Numeric Label'], bins=12, alpha = 0.4, fc=colors[i], ec='k', lw=0.25, label=f'Iteration {i}')
+			ax.axvline(x=-np.average(df['Numeric Label']), ymin=0, ymax=1, ls='--', c=colors[i], lw=0.75)
+			print(f'Mean B22 in Iter {i}:', np.average(df['Numeric Label']))
 	ax.legend()
 	ax.set_xlabel('$-B_{22}$', fontsize=13)
 	ax.set_ylabel('Frequency [per bin]', fontsize=13)
@@ -176,7 +246,7 @@ def plot_amino_acid_fractions(dataframe_list):
 	#x_positions = np.arange(0, num_iter*bar_width, bar_width)
 	x_pos = np.arange(0., 20.*bar_spacing, bar_spacing)
 	# choose discrete colors from a color map
-	cmap = matplotlib.colormaps['YlGn']
+	cmap = matplotlib.colormaps['YlGn_r']
 	colors = cmap(np.arange(0, 1, 1/num_iter), bytes=True)/255
 	# bytes=True gets you RGBA colors between 0-255, I divided by 255 to normalize
 
@@ -190,19 +260,24 @@ def plot_amino_acid_fractions(dataframe_list):
 	ax.tick_params(axis='both', which='major', length=6, labelsize=14)
 	ax.set_xlabel('Amino Acid', fontsize=14)
 	ax.set_ylabel('Fraction of Sequence', fontsize=14)
+
+	ax.text(x=0, y=0.55, s=r'$n_{\textrm{init}}$' + f'$ = {slice_after}$', fontsize=14)
 	plt.tight_layout()
-	plt.savefig('amino_acid_fractions.png', dpi=300)
+	plt.savefig(f'amino_acid_fractions_init{slice_after}.png', dpi=300)
 	plt.show()
 
 
 
 if __name__ == "__main__":
-	dataframe_list = []
-	for i, seq_set in enumerate([seqs_init, seqs_init50_r1, seqs_init50_r2, seqs_init50_r3]):
+	dataframe_list = [seqs_init, init50_r1_seqs, init50_r2_seqs,
+							  init50_r3_seqs, init50_r4_seqs, init50_r5_seqs]#[]
+	df_descriptors = []
+	for i, seq_set in enumerate(dataframe_list):#([seqs_init, init50_r1_seqs, init50_r2_seqs,
+							  #init50_r3_seqs, init50_r4_seqs, init50_r5_seqs]):#seqs_init50_r1, seqs_init50_r2, seqs_init50_r3]):
 		seq_set['Iteration'] = i*np.ones(len(seq_set))
-		dataframe_list.append(estimate_descriptors(seq_set))
-		
-	#make_plots(dataframe_list)
-	#compare_b22_distrib(dataframe_list)
-	plot_amino_acid_fractions(dataframe_list)
+		df_descriptors.append(estimate_descriptors(seq_set))
+	#make_plots(df_descriptors)
+	make_violin_plots(df_descriptors)
+	compare_b22_distrib(dataframe_list)
+	#plot_amino_acid_fractions(dataframe_list)
 	plt.show()
